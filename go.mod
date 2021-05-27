3737bc5f1a44381bc67b76c9a90f08414f4c919f
tosone <i@tosone.cn>
2021-05-27 15:17:57 +0800

    Add Banner

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/tosone/testgo
 
 go 1.15
+
+require (
+	github.com/dimiro1/banner v1.1.0
+	github.com/mattn/go-colorable v0.1.4
+)
--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
+// import (
+// 	"log"
+// 	"time"
+// )
+
+// func main() {
+// 	for {
+// 		log.Println("hello there")
+// 		<-time.After(time.Second * 5)
+// 	}
+// }
+
 import (
+	"fmt"
 	"log"
 	"time"
+
+	"github.com/dimiro1/banner"
+	"github.com/mattn/go-colorable"
 )
 
 func main() {
+	templ := `{{ .Title "Hello World" "" 0 }}
+   GoVersion: {{ .GoVersion }}
+   GOOS: {{ .GOOS }}
+   GOARCH: {{ .GOARCH }}
+   NumCPU: {{ .NumCPU }}
+   GOPATH: {{ .GOPATH }}
+   GOROOT: {{ .GOROOT }}
+   Now: {{ .Now "Monday, 2 Jan 2006" }}`
+	banner.InitString(colorable.NewColorableStdout(), true, true, templ)
+
+	fmt.Println()
+	fmt.Println()
+
 	for {
-		log.Println("hello there")
+		log.Println("hello there: ", time.Now().String())
 		<-time.After(time.Second * 5)
 	}
 }
